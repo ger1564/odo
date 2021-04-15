a87bd0c44e36a36a8eeaf633ac841fbe63b61b31
ger1564 <gexo1564@gmail.com>
2021-04-15 22:46:54 +0000

    [UPDATE]

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Odoo Academy',
+    'depends': ['base'],
+    'description': """
+                    Academy Module to manage Trainning:
+                    -Courses
+                    -Sessions
+                    -Attendees
+    """,
+    'category': 'Trainning',
+    'data': [
+	
+    ],
+    'demo': [
+	
+    ],
+    'application': True,
+
+}
