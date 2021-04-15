c27463c1a5f71e2cf61f706dd79fb241b8f821c9
ger1564 <gexo1564@gmail.com>
2021-04-15 23:05:45 +0000

    [UPDATE]

--- odoo_academy/models/course.py
+++ odoo_academy/models/course.py
@@ -6,11 +6,4 @@ class Course(models.Model):
 	_name = 'academy.course'
 	_description = 'Course Info'
 	
-	name = fields.Char(string='Title', required=True)
-	description = fields.Text(string='Description')
-	level = fields.Selection(String='Level',
-		selection=[('beginner','Beginner'),
-							('intermediate','Intermediate'),
-							('advanced','Advanced')], 
-							copy=False )
-	active = fields.Boolean(string='Active', default=True)
+
